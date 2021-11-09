d4069c0cab57c0652db4c5ab02daa4f5fa7a213a
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-11-09 11:19:33 -0300

    Rendering the movement

--- scripts/script.js
+++ scripts/script.js
@@ -31,37 +31,41 @@ function down() {
   }, velocity)
 }
 
-
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    if(!isJumping){
+    if (!isJumping) {
       jump();
     }
   }
 }
 
 
-function createCactus(){
+function createCactus() {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
+  let randomTime = (Math.random() * 3000) + cactusPosition ;
+
+  if (isGameOver) return
 
   cactus.classList.add('cactus');
   background.appendChild(cactus);
   cactus.style.left = cactusPosition + 'px';
 
   let leftTimer = setInterval(() => {
-    if(cactusPosition < -60){
+    if (cactusPosition < -60) {
       clearInterval(leftTimer);
       background.removeChild(cactus);
-    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
       clearInterval(leftTimer);
       isGameOver = true;
       document.body.innerHTML = '<h1 class="game-over">FIM DO JOGO</h1>';
     }
     cactusPosition -= steps;
     cactus.style.left = cactusPosition + 'px';
-      
-  }, velocity)
+
+  }, velocity);
+
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
