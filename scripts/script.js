87249a7e5081039d6ffe88937868b6b074ffcc7b
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-11-09 11:04:09 -0300

    Adding Game over

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
 const steps = 10;
 const velocity = 20;
@@ -48,13 +49,20 @@ function createCactus(){
   background.appendChild(cactus);
   cactus.style.left = cactusPosition + 'px';
 
-  setInterval(() => {
+  let leftTimer = setInterval(() => {
+    if(cactusPosition < -60){
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+      clearInterval(leftTimer);
+      isGameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">FIM DO JOGO</h1>';
+    }
     cactusPosition -= steps;
     cactus.style.left = cactusPosition + 'px';
       
   }, velocity)
 }
-
 
 createCactus();
 document.addEventListener('keyup', handleKeyUp);
